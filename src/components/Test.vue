984a36cd97d10409ad11cab94507e44d9e2a4cfa
WYY9515 <wangyuanyuan@supcon.com>
2020-05-10 22:13:13 +0800

    上传新建的仓库

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div @click="change">
+        {{val}}
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      val: 'great'
+    }
+  },
+  methods: {
+    change () {
+      console.log(this.val)
+      console.log(111)
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
